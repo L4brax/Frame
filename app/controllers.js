e2158bd8d18a3b4768527084ad4735782f013135
Hess <antoine.hess@accenture.com>
2016-06-28 18:19:37 +0200

    Added raw datas and shaped the templates

--- app/controllers.js
+++ app/controllers.js
@@ -1,7 +1,39 @@
 app.controller("pathCtrl", function($scope){
-  $scope.msg1 = "Parcours No 1";
+  $scope.paths = {
+    path1 : {
+      place: "Université de Nantes",
+      degree : "Licence MIAGE",
+      started : "08-2015",
+      ended : "08-2015",
+      discipline : "Java, C, Méthodes UML, Conduite de projets."
+    },
+    path2 : {
+      place: "Université de Nantes",
+      degree : "Master MIAGE",
+      started : "08-2015",
+      ended : "08-2015",
+      discipline : "Java, C, Méthodes UML, Conduite de projets."
+    }
+  };
 });
 
 app.controller("expCtrl", function($scope){
-  $scope.msg1 = "Exp No 1";
+  $scope.experiences = {
+    exp1 : {
+      position : "Developer",
+      company : "Accenture",
+      started : "04-2015",
+      ended : "08-2015",
+      place : "Nantes",
+      description : "Java development"
+    },
+    exp2 : {
+      position : "Developer Analyst",
+      company : "Accenture",
+      started : "04-2016",
+      ended : "07-2015",
+      place : "Nantes",
+      description : "Java development"
+    }
+  };
 });
