59f1bc471e5b427d4dc30f4e4a7e9d9b0173cee1
Hess <antoine.hess@accenture.com>
2016-06-28 16:25:27 +0200

    Shaping routing. Added controllers and module.

---
+++ app/controllers.js
@@ -0,0 +1,7 @@
+app.controller("pathCtrl", function($scope){
+  $scope.msg1 = "Parcours No 1";
+});
+
+app.controller("expCtrl", function($scope){
+  $scope.msg1 = "Exp No 1";
+});
--- app/module.js
+++ app/module.js
@@ -1,1 +1,15 @@
-var app = angular.module('frame', []);
+var app = angular.module('frame', ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/parcours", {
+        templateUrl : "app/templates/path.html",
+        controller : "pathCtrl"
+    })
+    .when("/experiences", {
+        templateUrl : "app/templates/exp.html",
+        controller : "expCtrl"
+    })
+    .otherwise({
+      templateUrl : "app/templates/main.html"
+    });
+});
