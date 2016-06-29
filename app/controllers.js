fdbdfc5305cbe07364686b8c988deb128b72e634
Hess <antoine.hess@accenture.com>
2016-06-29 18:03:41 +0200

    Adding CSS, shaping http requests and json files.

--- app/controllers.js
+++ app/controllers.js
@@ -1,39 +1,38 @@
-app.controller("pathCtrl", function($scope){
-  $scope.paths = {
-    path1 : {
-      place: "Université de Nantes",
-      degree : "Licence MIAGE",
-      started : "08-2015",
-      ended : "08-2015",
-      discipline : "Java, C, Méthodes UML, Conduite de projets."
-    },
-    path2 : {
-      place: "Université de Nantes",
-      degree : "Master MIAGE",
-      started : "08-2015",
-      ended : "08-2015",
-      discipline : "Java, C, Méthodes UML, Conduite de projets."
-    }
-  };
+app.controller("pathCtrl", function($scope, $http){
+  $http.get("data/frame.json").then(function(response){
+    $scope.paths = response.data.paths;
+  });
+  // $scope.paths = [{
+  //   place: "Header1",
+  //   degree : "Header2",
+  //   started : "08-02-2015",
+  //   ended : "08/2015",
+  //   discipline : "Disc1, disc2..."
+  // },
+  // {
+  //   place: "Header1",
+  //   degree : "Header2",
+  //   started : "08-2015",
+  //   ended : "08-2015",
+  //   discipline : "Disc1, disc2..."
+  // }];
 });
 
 app.controller("expCtrl", function($scope){
-  $scope.experiences = {
-    exp1 : {
-      position : "Developer",
-      company : "Accenture",
-      started : "04-2015",
-      ended : "08-2015",
-      place : "Nantes",
-      description : "Java development"
-    },
-    exp2 : {
-      position : "Developer Analyst",
-      company : "Accenture",
-      started : "04-2016",
-      ended : "07-2015",
-      place : "Nantes",
-      description : "Java development"
-    }
-  };
+  $scope.experiences = [{
+    position : "Header1",
+    company : "Header2",
+    started : "04-2015",
+    ended : "08-2015",
+    place : "Place",
+    description : "Lorem ipsum"
+  },
+  {
+    position : "Header1",
+    company : "Header2",
+    started : "04-2016",
+    ended : "07-2015",
+    place : "Place2",
+    description : "Lorem Ipsum"
+  }];
 });
