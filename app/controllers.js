455e62e0226d6cd340a6ee96c608d76d2370535c
Hess <antoine.hess@accenture.com>
2016-06-30 18:02:47 +0200

    Shaping router with routerParams

--- app/controllers.js
+++ app/controllers.js
@@ -1,38 +1,6 @@
 app.controller("pathCtrl", function($scope, $http){
   $http.get("data/frame.json").then(function(response){
-    $scope.paths = response.data.paths;
+    $scope.paths = response.data;
+    console.log($scope.paths);
   });
-  // $scope.paths = [{
-  //   place: "Header1",
-  //   degree : "Header2",
-  //   started : "08-02-2015",
-  //   ended : "08/2015",
-  //   discipline : "Disc1, disc2..."
-  // },
-  // {
-  //   place: "Header1",
-  //   degree : "Header2",
-  //   started : "08-2015",
-  //   ended : "08-2015",
-  //   discipline : "Disc1, disc2..."
-  // }];
-});
-
-app.controller("expCtrl", function($scope){
-  $scope.experiences = [{
-    position : "Header1",
-    company : "Header2",
-    started : "04-2015",
-    ended : "08-2015",
-    place : "Place",
-    description : "Lorem ipsum"
-  },
-  {
-    position : "Header1",
-    company : "Header2",
-    started : "04-2016",
-    ended : "07-2015",
-    place : "Place2",
-    description : "Lorem Ipsum"
-  }];
 });
--- app/module.js
+++ app/module.js
@@ -1,13 +1,13 @@
 var app = angular.module('frame', ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/parcours", {
+    .when("/path/:name", {
         templateUrl : "app/templates/path.html",
         controller : "pathCtrl"
     })
-    .when("/experiences", {
+    .when("/Item2", {
         templateUrl : "app/templates/exp.html",
-        controller : "expCtrl"
+        controller : "pathCtrl"
     })
     .otherwise({
       templateUrl : "app/templates/main.html"
