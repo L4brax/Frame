98fe864018217694370c5030f462dd9803a4fd3e
Hess <antoine.hess@accenture.com>
2016-07-01 18:06:21 +0200

    Adding main.json and completed routing with controller.

--- app/controllers.js
+++ app/controllers.js
@@ -1,6 +1,23 @@
 app.controller("pathCtrl", function($scope, $http){
   $http.get("data/frame.json").then(function(response){
-    $scope.paths = response.data;
-    console.log($scope.paths);
+    $scope.paths = response.data.frame;
+  });
+  $http.get("data/main.json").then(function(response){
+    $scope.mainItems = response.data.main;
+  });
+});
+
+app.controller("contentCtrl", function($scope, $http, $routeParams){
+  $http.get("data/frame.json").then(function(response){
+    var frameData = response.data.frame;
+    var content = function (frameDataf, routeParams) {
+      for (var i = 0; i < frameDataf.length;i++){
+        if (frameDataf[i].name == routeParams.name) {
+          return frameDataf[i].content;
+        }
+      };
+    }
+    $scope.itemContent = content(frameData,$routeParams);
+    $scope.itemName = $routeParams.name;
   });
 });
--- app/module.js
+++ app/module.js
@@ -1,13 +1,9 @@
-var app = angular.module('frame', ["ngRoute"]);
+  var app = angular.module('frame', ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/path/:name", {
         templateUrl : "app/templates/path.html",
-        controller : "pathCtrl"
-    })
-    .when("/Item2", {
-        templateUrl : "app/templates/exp.html",
-        controller : "pathCtrl"
+        controller : "contentCtrl"
     })
     .otherwise({
       templateUrl : "app/templates/main.html"
